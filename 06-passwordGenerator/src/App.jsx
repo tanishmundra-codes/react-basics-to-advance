9532d21f8d769dd58446405079d04f75d2eff07a
tanishmundra-codes <tanishmundra2@gmail.com>
2025-07-11 21:04:04 +0530

    Creating password generator

---
+++ 06-passwordGenerator/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [len, setLen] = useState(8);
+  const [num, setNum] = useState(false);
+  const [char, setChar] = useState(false);
+  const [pass, setPass] = useState("");
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
